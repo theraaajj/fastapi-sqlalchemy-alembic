2e7e3fb19e167e9182a57c812b50a5cc15c6ceeb
RAJ ARYAN <114001897+theraaajj@users.noreply.github.com>
2025-10-06 10:51:38 +0530

    Update PEP8 database.py

--- app/database.py
+++ app/database.py
@@ -2,14 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-## this should match our alembic.ini file
-DATABASE_URL = "postgresql://postgres:db_pswrd@localhost/first-db"
+# This should match our alembic.ini file
+DATABASE_URL = "postgresql://postgres:thisisme2010@localhost/first-db"
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
 
 # Dependency to get a DB session
 def get_db():
@@ -18,3 +19,10 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+"""
+This file sets up the connection to the PostgreSQL database and creates
+a session-making utility that the rest of the application uses to talk to
+the database.
+"""
