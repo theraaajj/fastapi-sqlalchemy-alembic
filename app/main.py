d37e866a9fff8d0fc12450f867d4b2a6af804c68
RAJ ARYAN <114001897+theraaajj@users.noreply.github.com>
2025-10-06 10:52:26 +0530

    Update PEP8 main.py

--- app/main.py
+++ app/main.py
@@ -7,11 +7,14 @@ from .database import get_db
 
 app = FastAPI()
 
+
+# To get/read users by ID
 @app.get("/")
 def read_root():
     return {"message": "This is Raj, and this is a simple API for user management built with FastAPI, PostgreSQL, and Alembic for database migrations."}
 
 
+# To create users
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
@@ -20,12 +23,14 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 
+# To get/read all users
 @app.get("/users/", response_model=List[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
+# To get/read users by ID
 @app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
@@ -34,6 +39,7 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
+# To update users
 @app.put("/users/{user_id}", response_model=schemas.User)
 def update_user_details(user_id: int, user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.update_user(db, user_id=user_id, user_update=user)
@@ -42,6 +48,7 @@ def update_user_details(user_id: int, user: schemas.UserCreate, db: Session = De
     return db_user
 
 
+# To delete users
 @app.delete("/users/{user_id}", response_model=schemas.User)
 def delete_user_details(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.delete_user(db, user_id=user_id)
