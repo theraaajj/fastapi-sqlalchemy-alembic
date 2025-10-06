887ee7af21062946a9ee73a4e5e52d224cc301d0
RAJ ARYAN <114001897+theraaajj@users.noreply.github.com>
2025-10-06 10:53:37 +0530

    Update PEP8 models.py

--- app/models.py
+++ app/models.py
@@ -3,14 +3,23 @@ from sqlalchemy.sql import func
 
 from .database import Base
 
+
+# User model
 class User(Base):
     __tablename__ = "users"
 
+    # Initial columns
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
-    
-    # These columns will be added in the second migration
+
+    # These columns were added in the second migration
     phone_number = Column(String, nullable=True)
     address = Column(String, nullable=True)
+
+
+"""
+Defines the database table structure using SQLAlchemy's ORM
+[database tables as Python classes]
+"""
